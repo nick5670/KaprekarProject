4fd5ae5de18e170ba063870e9074fd610e74e3b0
nick5670 <42557441+nick5670@users.noreply.github.com>
2018-09-06 23:33:27 -0500

    Changed variable name
    Fixed the spelling of modulus so the other variables are defined

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -31,7 +31,7 @@ int isKaprekar(int n) {
   int i;
   long square = n * (long) n;
   int numDigits = (int) log10(n) + 1;
-  long modulous = 0;
+  long modulus = 0;
   long first, second;
 
   //for each possible "split" of the square...
